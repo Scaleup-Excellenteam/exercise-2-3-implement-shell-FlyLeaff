c61051bad2ed5e4fd370e39b73e89f68d49e780c
FlyLeaff <kohav24@gmail.com>
2024-05-17 22:36:54 +0300

    commit 1

--- src/header files/Command.h
+++ src/header files/Command.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;	
+
+class Command
+{
+
+	public:
+		Command(string command);
+		~Command();
+		void parse(string& input);
+
+		void run();
+
+	private:
+		string command;
+		vector<string> args;
+
+
+
+
+};
